4e3f1221256b8cb98306c6c02b5591b147059a80
Brandon Rowe <brandonhrowe@gmail.com>
2019-07-11 15:37:18 -0400

    modified settings for postgres db

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_blog',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
